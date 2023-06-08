f57026f68c38c5398c235781d35632e0e6bfaa35
Jarweb01 <jarvisweb@mail.ru>
2023-06-08 22:11:31 +0300

    Add page for project

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const clientConfig = {
+    projectId: 'odr04iai',
+    dataset: 'production',
+    apiVersion: '2023-06-07',
+}
+
+export default clientConfig;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,14 +1,9 @@
 import { createClient, groq } from "next-sanity";
 import { Project } from "@types/project";
+import clientConfig from './config/client-config';
 
 export async function getProjects(): Promise<Project[]> {
-    const client = createClient({
-        projectId: 'odr04iai',
-        dataset: 'production',
-        apiVersion: '2023-06-07',
-    });
-
-    return client.fetch(
+    return createClient(clientConfig).fetch(
         groq`*[_type == "project"] {
             _id,
             _createdAt,
@@ -18,5 +13,20 @@ export async function getProjects(): Promise<Project[]> {
             url,
             content
         }`
+    )
+}
+
+export async function getProject(slug:string): Promise<Project>{
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "project" && slug.current == $slug][0] {
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`,
+        { slug }
     )
 }
